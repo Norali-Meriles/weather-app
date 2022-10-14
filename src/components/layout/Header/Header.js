bdc57bf75e563671a147d6a65929c56c2f9d233e
Norali Meriles <merilesnorali.1996@gmail.com>
2022-10-14 20:28:49 -0300

    home layout design

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,20 +1,22 @@
 const Header = () => {
   return (
-    <nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
-      <div className='container'>
-        <a className='navbar-brand' href='#'><i className='bi bi-cloud-lightning-rain-fill pe-2 text-primary' /> Weather-App</a>
-        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse d-flex justify-content-end' id='navbarNav'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <button className='btn'><i className='bi bi-brightness-low text-light' /></button>
-            </li>
-          </ul>
+    <header>
+      <nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
+        <div className='container'>
+          <a className='navbar-brand' href='#'><i className='bi bi-cloud-lightning-rain-fill pe-2 text-primary' /> Weather-App</a>
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse justify-content-end' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <button className='btn'><i className='bi bi-brightness-low text-light' /></button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,81 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='container'>
+      <div className='row'>
+        <aside className='col-4 d-flex flex-column bg-secondary'>
+          <div className='d-flex flex-row justify-content-between py-3'>
+            <button className='btn btn-primary'><i className='bi bi-search-heart pe-2' />Search</button>
+            <button className='btn btn-primary'><i className='bi bi-geo' /></button>
+          </div>
+          <div className='d-flex flex-column justify-content-between h-100 py-3'>
+            <img className='w-100' src='https://picsum.photos/200' />
+            <div className='d-flex flex-column justify-content-end align-items-center'>
+              <p className='fs-1'>16 °C</p>
+              <p className="fs-3">Shower</p>
+              <p>Today - 14 octu</p>
+              <p>Tucuman</p>
+            </div>
+          </div>
+        </aside>
+        <div className='col-8 bg-danger'>
+          <div className='d-flex flex-row justify-content-end my-3'>
+            <button className='btn btn-primary me-2'><i className='bi bi-c-circle' /></button>
+            <button className='btn btn-primary me-4'>F</button>
+          </div>
+          <div className='d-flex flex-row justify-content-evenly my-3'>
+            <div className='card w-25'>
+              <img src='https://picsum.photos/200' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the content.</p>
+              </div>
+            </div>
+            <div className='card w-25'>
+              <img src='https://picsum.photos/200' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the content.</p>
+              </div>
+            </div>
+            <div className='card w-25'>
+              <img src='https://picsum.photos/200' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="container my-3">
+            <h3 className='fs-4'>Todays Hihgthikds </h3>
+            <div className="d-flex row justify-content-evenly my-3">
+              <div className='card w-25 mx-1'>
+                <div className='card-body'>
+                  <h5 className='card-title'>Card title</h5>
+                  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                </div>
+              </div>
+              <div className='card  w-25 mx-1'>
+                <div className='card-body'>
+                  <h5 className='card-title'>Card title</h5>
+                  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex row justify-content-evenly my-3">
+              <div className='card w-25 mx-1'>
+                <div className='card-body'>
+                  <h5 className='card-title'>Card title</h5>
+                  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                </div>
+              </div>
+              <div className='card  w-25 mx-1'>
+                <div className='card-body'>
+                  <h5 className='card-title'>Card title</h5>
+                  <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section >
   );
 };
 
